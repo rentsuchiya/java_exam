ebb08a76d04cf4963b9810933d6b190a710e44d9
REN <ren.tsuchiya@rakus-partners.co.jp>
2026-01-16 11:10:50 +0900

    プログラミング入門２

---
+++ 1-1.test-java-entry1/src/en9toi1.java
@@ -0,0 +1,8 @@
+public class en9toi1 {
+    public static void main(String[] args) {
+        printName();
+    }
+   static void printName(){
+    System.out.println("土屋蓮");
+   }
+}
---
+++ 1-2.test-java-entry2/src/Exam2.java
@@ -0,0 +1,5 @@
+public class Exam2 {public static void main(String[] args) {
+    while 
+}
+
+}
