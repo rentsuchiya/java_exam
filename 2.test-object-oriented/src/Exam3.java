444bce5fa5a589a83de68b81983518d045781a9e
REN <ren.tsuchiya@rakus-partners.co.jp>
2026-01-20 09:48:39 +0900

    オブジェクト指向入門

---
+++ 2.test-object-oriented/src/Exam3.java
@@ -0,0 +1,10 @@
+public class Exam3 {
+    public static void main(String[] args) {
+        Student student =new Student();
+        student.study();
+        student.study();
+        student.sleep();
+        student.sleep();
+    }
+
+}
