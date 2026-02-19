d2771100ea390736e94e6d5f25a42382b2d72b71
REN <ren.tsuchiya@rakus-partners.co.jp>
2026-02-19 10:09:26 +0900

    spring JDBC

---
+++ 1-4.test-spring-jdbc-tsuchiya/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	@Autowired
+	private MemberRepository repository;
+
+	@RequestMapping("")
+	public String index() {
+		Member member = new Member();
+		member = repository.load(2);
+		System.out.println(member);
+		List<Member> member2 = repository.findAll();
+		System.out.println(member2);
+
+		return "member";
+	}
+}
